bfcb60b2852b49b475c8dd3fe9153d829ce4b644
benardii <113827453+benbal22@users.noreply.github.com>
2024-03-06 10:11:19 +0100

    Add files via upload
    Iphone 5c
    
    Signed-off-by: benardii <113827453+benbal22@users.noreply.github.com>

---
+++ 17.02.2024/file.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class file 
+{
+	public static void main(String argv[])throws IOEXception
+	{
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+
+		int zahl;
+
+		System.out.println("Schreibe eine Zahl")
+	}
+}
